7cecc9a103490498b05b245e21d2afbbeeb2801c
AlbyShelly <albyshelly4@gmail.com>
2023-11-27 15:12:26 +0530

    python read and write file program done

---
+++ s1/python_lab/c05/readfile.py
@@ -0,0 +1,62 @@
+
+def write_to_file(file_path, content):
+
+    with open(file_path, 'w') as file:
+
+        file.write(content)
+
+
+def read_file_to_list(file_path):
+
+    lines = []
+
+    with open(file_path,'r') as file:
+
+        for line in file:
+
+            lines.append(line.strip())
+
+
+        return lines
+
+
+file_path = 'example.txt'
+
+content_to_write = """Line 1: This is the first line.
+Line 2: This is the second ilne.
+Line 3: This is the third line."""
+
+write_to_file(file_path, content_to_write)
+
+lines_read = read_file_to_list(file_path)
+
+print("Lines read from the file: ", file_path)
+
+for line in lines_read:
+
+    print(line)
+
+def copy_odd_lines(input_file_path, output_file_path):
+   
+    counter = 1 
+
+    with open(input_file_path, 'r') as input_file:
+
+        with open(output_file_path, 'w') as output_file:
+
+            for line in input_file:
+
+                print("Line of the file :",line)
+
+                if counter % 2 != 0:
+
+                    output_file.write(line)
+
+                counter += 1
+
+input_file_path = 'example.txt'
+
+output_file_path = 'newexample.txt'
+
+
+copy_odd_lines(input_file_path, output_file_path)
